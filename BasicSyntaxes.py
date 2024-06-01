c2197f29008f591f742b611fb26f05acd32e660c
Sumanth Soora <geolight03@gmail.com>
2024-06-01 12:52:45 -0400

    changed th efile name n added few more basic syntax's

---
+++ BasicSyntaxes.py
@@ -0,0 +1,17 @@
+print ("hello World!")
+
+print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.")
+print("2. Knead the dough for 10 minutes.")
+print("3. Add 3g of Salt.")
+print("4. Leave to rise for 2 hours.\n5. Bake at 200 degrees C for 30 minutes.")
+
+#concatination 
+print("Sumanth"+" "+"Soora")
+
+#input function
+x=input("what is your name:")    #input reads adn stores the value into it for you inorder 
+print('hello!' + x)                     #to use you need to store it a differerent variable
+
+num1=input()
+num2=input()
+print(num1+num2)
