761ea4df23acaa20094620ff97d7241f4a173ed8
Sumanth Soora <geolight03@gmail.com>
2024-06-04 16:15:10 -0400

    added a leapyear program

---
+++ leapyear.py
@@ -0,0 +1,13 @@
+year=int(input("enter the year you wanna find out if it's a leap year or not:"))
+if year % 4 == 0:
+  if year % 100 == 0:
+    if year % 400 == 0:
+      print("Leap year")
+    else:
+      print("Not leap year")
+  else:
+    print("Leap year")
+else:
+  print("Not leap year")
+
+
