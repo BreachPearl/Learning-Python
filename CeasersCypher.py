17a1a64253bd3b0d426f1e555f509fa753c6c3bd
Sumanth Soora <geolight03@gmail.com>
2024-06-06 13:00:51 -0400

    Finished the programing of ceasers encryption

--- CeasersCypher.py
+++ CeasersCypher.py
@@ -1,17 +1,20 @@
 import string
-user_input=input("enter 'encode' for encryption and 'decode' for decrypt\n")
-def encryption():
-    code=input("enter your message: \n")
-    shift=int(input("type the shift number: \n"))
-    first_set=list(string.ascii_lowercase)
-    shifted_set=list(string.ascii_lowercase)
-    i=0
-    while i<(26-shift):
-        shifted_set[i]=first_set[shift+i]
-        i=+1
-    print(shifted_set)
-if user_input=="encode":
-    encryption()
-# if user_input=="decode":
-#     decryption()
-string.
+import itertools
+
+def Ceasers(text,shiftby,cipher_direction):
+    alphabet_set=list(string.ascii_lowercase+string.ascii_lowercase)
+    end_text=""
+    if cipher_direction=="decode":
+            shiftby=shiftby*-1
+    for i in text:
+        position=alphabet_set.index(i)
+        new_position=position+shiftby
+        end_text+=alphabet_set[new_position]
+    print(f"the {cipher_direction}d text is {end_text}")
+
+direction=input("enter 'encode' for encryption and 'decode' for decrypt\n").lower()
+code=(input("enter your message: \n")).lower()
+shift=int(input("type the shift number: \n"))
+
+Ceasers(code,shift,direction)
+
